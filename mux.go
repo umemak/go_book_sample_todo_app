96569781b6ab88226dfd0b45503d5118f2bdae8a
umemak <umematsu@gmail.com>
2022-08-20 15:40:37 +0900

    add userregister

--- mux.go
+++ mux.go
@@ -34,5 +34,10 @@ func NewMux(ctx context.Context, cfg *config.Config) (http.Handler, func(), erro
 		Service: &service.ListTask{DB: db, Repo: &r},
 	}
 	mux.Get("/tasks", lt.ServerHTTP)
+	ru := &handler.RegisterUser{
+		Service:   &service.RegisterUser{DB: db, Repo: &r},
+		Validator: v,
+	}
+	mux.Post("/register", ru.ServeHTTP)
 	return mux, cleanup, nil
 }
